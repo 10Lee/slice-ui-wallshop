5e3d75ea7a54fa8b9a1252b1f160074b6fe3e457
10Lee <mail.10lee@gmail.com>
2022-05-25 13:48:33 +0800

    complete

---
+++ lib/models/populer_model.dart
@@ -0,0 +1,13 @@
+class PopulerModel {
+  final String imageUrl;
+  final String title;
+  final String price;
+  final String discPrice;
+
+  PopulerModel({
+    required this.imageUrl,
+    required this.title,
+    required this.price,
+    required this.discPrice,
+  });
+}
